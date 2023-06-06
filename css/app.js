52dbf28ca8dacc0fb9567e46d763f8dc92d267fd
root <root@DESKTOP-6I3RBRC>
2023-06-06 23:29:58 +0300

    VAT Assignment

---
+++ css/app.js
@@ -0,0 +1,28 @@
+//first method 
+
+const vat= 0.16
+
+{
+
+function calculateinclusivePrice(exclusivePrice)
+{
+const inclusivePrice= (vat * exclusivePrice) + exclusivePrice
+console.log(inclusivePrice)
+
+}
+
+calculateinclusivePrice(200)
+
+}
+
+// second method
+
+{
+const exclusivePrice = 200
+
+{
+const inclusivePrice= (vat * exclusivePrice) + exclusivePrice
+console.log(inclusivePrice)
+
+}
+}
